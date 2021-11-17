539c7257006e0c6a78ee4b6151cefb166a41f2f6
Hok Vicheka <hvicheka.it@gmail.com>
2021-11-17 18:31:45 +0700

    Video Encoding using Laravel Command

---
+++ app/Console/Commands/VideoEncode.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands;
+
+use FFMpeg\Format\Video\X264;
+use Illuminate\Console\Command;
+use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg;
+
+class VideoEncode extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'video-encode:start';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $low = (new X264('aac'))->setKiloBitrate(500);
+        $high = (new X264('aac'))->setKiloBitrate(1000);
+
+        FFMpeg::fromDisk('video-temp')
+            ->open('7KFHml0Yj5ZFUC7Je2v6ZUbSYNJQD2BYOZODNW9E.mp4')
+            ->exportForHLS()
+            ->addFormat($low, function ($filters) {
+                $filters->resize(640, 480);
+            })
+            ->addFormat($high, function ($filters) {
+                $filters->resize(1280, 720);
+            })
+            ->onProgress(function ($progress) {
+                $this->info("Progress= {$progress}%");
+            })
+            ->toDisk('video-temp')
+            ->save('/test/file.m3u8');
+    }
+}
