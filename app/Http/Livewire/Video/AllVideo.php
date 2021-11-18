f7697742691fa701bf826755f070a30134a3228a
Hok Vicheka <hvicheka.it@gmail.com>
2021-11-18 13:49:29 +0700

    get all videos

--- app/Http/Livewire/Video/AllVideo.php
+++ app/Http/Livewire/Video/AllVideo.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Livewire\Video;
 
+use App\Models\Channel;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class AllVideo extends Component
 {
+    use WithPagination;
+
+    public $video;
+    public $channel;
+
+    protected $paginationTheme = 'bootstrap';
+
+    public function mount(Channel $channel)
+    {
+        $this->channel = $channel;
+    }
+
     public function render()
     {
-        return view('livewire.video.all-video');
+        $videos = auth()->user()->channel->videos()->paginate(10);
+        return view('livewire.video.all-video', [
+            'videos' => $videos
+        ])
+            ->extends('layouts.app');
     }
 }
--- resources/views/livewire/video/all-video.blade.php
+++ resources/views/livewire/video/all-video.blade.php
@@ -1,3 +1,40 @@
-<div>
-    The best athlete wants his opponent at his best.
+<div class="container">
+    <div class="justify-content-center">
+        @forelse($videos as $video)
+            <div class="card my-2">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <img src="{{ $video->thumbnail }}" class="img-thumbnail" width="100" height="100"
+                                 alt="No Image">
+                        </div>
+                        <div class="col-md-3">
+                            <h5>{{ $video->title }}</h5>
+                            <p class="text-truncate">{{ $video->description }}</p>
+                        </div>
+                        <div class="col-md-2">
+                            {{ $video->visibility }}
+                        </div>
+                        <div class="col-md-2">
+                            {{ $video->created_at->format('m-d-Y') }}
+                        </div>
+                        <div class="col-md-2">
+                            <a href="{{ route('videos.edit',['channel' => auth()->user()->channel,'video' => $video]) }}"
+                               class="btn btn-sm btn-primary">Edit</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @empty
+            <div class="card my-2">
+                <div class="card-body text-center">
+                    No Video
+                </div>
+            </div>
+        @endforelse
+
+    </div>
+    <div class="row justify-content-center mx-auto">
+        {{ $videos->links() }}
+    </div>
 </div>
