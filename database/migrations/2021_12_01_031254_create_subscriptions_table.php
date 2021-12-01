d5d07da0bc6406be912dcab93d5c1143c655462c
Hok Vicheka <hvicheka.it@gmail.com>
2021-12-01 10:54:42 +0700

    Channel Subscription

---
+++ database/migrations/2021_12_01_031254_create_subscriptions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignUuid('user_id')->constrained();
+            $table->foreignUuid('channel_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
